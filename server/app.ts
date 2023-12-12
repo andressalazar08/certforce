332af74b2587f57a46dcfddaca85ecfe85f67d7d
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-12-12 17:28:19 -0500

    app middlewares

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,32 @@
-import express from "express";
-export const app= express();
+import express, { Request, Response, NextFunction } from "express";
+export const app = express();
+require("dotenv").config();
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//body parser
+app.use(express.json({ limit: "5mb" }));
+
+//cookie parser
+app.use(cookieParser());
+
+//cors
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
