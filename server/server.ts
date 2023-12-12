2628dab33e00cf535b0e0ade7cbe0c0023699b88
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-12-12 18:17:41 -0500

    db and redis config

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,11 @@
 import { app } from "./app";
 require("dotenv").config();
-
+import connectDB from "./utils/db";
 
 //create a server
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is conected with port: ${process.env.PORT}`);
+    connectDB();
 });
 
 
---
+++ server/utils/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+require("dotenv").config();
+
+const dburl:string = process.env.DB_URL || '';
+
+const connectDB = async() =>{
+    try{
+        await mongoose.connect(dburl).then((data:any)=>{
+            console.log(`Database connected with ${data.connection.host}`)
+        })
+    }catch(error:any){
+        console.log(error.messsage);
+        setTimeout(connectDB,5000);
+
+    }
+}
+
+export default connectDB;
